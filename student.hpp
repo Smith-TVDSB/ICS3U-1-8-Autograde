0b44fb54d874338e2363384c2661693f22e5dff5
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-08-17 15:42:33 -0400

    Uploaded necessary files

---
+++ student.hpp
@@ -0,0 +1,43 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+
+const float PI = 3.14159;
+
+// Exercise 1: implements function 'add', that adds two numbers
+int add(int a, int b);
+
+// Exercise 2: remainder calculator
+int remainder(int a, int b);
+
+// Exercise 3: Circumference function
+float circumference(float r);
+
+//Exercise 4: Concatenate a full name with middle initial
+string concat(string firstName, char middleInitial, string lastName);
+
+// ===============
+// implementations
+
+int add(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
+// Exercise 2: implements function that calculates remainder
+int remainder(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
+//Exercise 3: implements function that calculates circumference
+float circumference(float r)
+{
+	return 0; //TODO: fix
+}
+
+//Exercise 4: concate first name, middle initial, and last name in proper format
+string concat(string firstName, char middleInitial, string lastName)
+{
+	return "nothing";//TODO: fix
+}
