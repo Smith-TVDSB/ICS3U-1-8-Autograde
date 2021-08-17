8777978e64ce08e32fa4c907bf2d48d2805375a5
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-08-17 15:59:45 -0400

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -27,17 +27,17 @@ int add(int a, int b)
 // Exercise 2: implements function that calculates remainder
 int remainder(int a, int b)
 {
-   return 0; // TODO: fix
+   return a%b; // TODO: fix
 }
 
 //Exercise 3: implements function that calculates circumference
 float circumference(float r)
 {
-	return 0; //TODO: fix
+	return 3.14159*r*2; //TODO: fix
 }
 
 //Exercise 4: concate first name, middle initial, and last name in proper format
 string concat(string firstName, char middleInitial, string lastName)
 {
-	return "nothing";//TODO: fix
+	return firstName+" "+middleInitial+". "+lastName;//TODO: fix
 }
